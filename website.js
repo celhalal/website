5f6102f41ce754488292d4047e88150625a2499d
celhalal <cathyelhalal@gmail.com>
2022-05-26 12:56:13 -0400

    final draft

--- website.js
+++ website.js
@@ -2,36 +2,49 @@
 const internInfo = {
     name: 'Cathy El-Halal',
     role: 'Development Intern',
-    history: 'Cathy El-Halal is currently a junior doing a double major in Computer Science and Business at Wheaton College, MA. Cathy has a great interest in full-stack development and artificual intelligence.',
-    langList: 'Some of the technologies Cathy has been working with:',
-    languages: ['C++', 'Python', 'SQL', 'MIPS', 'JavaScript', 'CSS', 'HTML', 'Shell Script']
+    history: ['Cathy El-Halal is currently a junior doing a double major in Computer Science and Business at Wheaton College, MA. Cathy has a great interest in full-stack development and artificual intelligence.','Some of the technologies Cathy has been working with:'],
+    languages: ['C++', 'Python', 'SQL', 'MIPS', 'JavaScript', 'CSS', 'HTML', 'Shell Script'],
+    credits: ['Built and Designed by Cathy El-Halal', 'All rights reserved. ©']
 };
 
+
 // insert name into h3
-let title = document.getElementById('name');
-let h3 = document.createElement('h3');
-h3.innerText = internInfo.name;
-title.appendChild(h3);
+const data = document.getElementById("metadata");
+const name = document.createElement('div');
+name.id = 'name';
+name.innerText = internInfo.name;
+data.appendChild(name);
 
-// inster role name into h4
-let role = document.getElementById('title');
-let h4 = document.createElement('h4');
-h4.innerText = internInfo.role;
-title.appendChild(h4);
+// insert role name into h4
+const role = document.createElement('div');
+role.id = 'role';
+role.innerText = internInfo.role;
+data.appendChild(role);
 
-// insert history & language List into h5
-let strings = document.getElementById('parg1');
-let h5 = document.createElement('h5');
-h5.innerText = internInfo.history;
-strings.appendChild(h5);
-
-
-
+// insert texts (history)
+var parg = document.getElementById('description');
+internInfo.history.forEach((el)=>{
+    var div = document.createElement('div');
+    div.innerText = el;
+    parg.appendChild(div);
+    var newLine = document.createElement('br');
+    parg.appendChild(newLine);
+})
 
 // insert ul of languages
-let list = document.getElementById('teck-stack');
+var languages = document.getElementById('list');
 internInfo.languages.forEach((el)=>{
-    let li = document.createElement('li');
-    li.innerText = el;
-    list.appendChild(li);
+    const li = document.createElement('li');
+    li.innerText= el;
+    languages.appendChild(li);
 });
+
+// insert credits
+var final = document.getElementById('footer');
+internInfo.credits.forEach((el)=>{
+    var newLine = document.createElement('br');
+    final.appendChild(newLine);
+    var foot = document.createElement('footer');
+    foot.innerText = el;
+    final.appendChild(foot);
+})
